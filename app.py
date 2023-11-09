e52082012f97e2c6964db1e20d9775f871e94397
Zanzigzan <36047075+Zanzigzan@users.noreply.github.com>
2023-11-09 14:02:46 -0800

    App insert hello message

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-import pymongo
-import json
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-connectionToMongo = pymongo.MongoClient("mongodb+srv://ProjectCoucou:THEpassword>@cluster0.ekjezah.mongodb.net/?retryWrites=true&w=majority")
-db = connectionToMongo["Coucou"]
+uri = "mongodb+srv://ProjectCoucou:THEproject@cluster0.ekjezah.mongodb.net/?retryWrites=true&w=majority"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+db = client["Coucou"]
 dbcollection = db["CoucouFiles"]
+
+# Send a ping to confirm a successful connection
+try:
+    data = {
+        "Message": "Hello!"
+    }
+
+    dbcollection.insert_one(data)
+except Exception as e:
+    print(e)
