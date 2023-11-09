1f28443f90c289e4bfa6a67edf7555a29d26bf46
Victor Campos <victormscampos@uol.com.br>
2023-11-09 13:53:53 -0800

    working mongoDB

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import pymongo
 import json
 
 connectionToMongo = pymongo.MongoClient("mongodb+srv://ProjectCoucou:THEpassword>@cluster0.ekjezah.mongodb.net/?retryWrites=true&w=majority")
-db = dbconnection["Coucou"]
+db = connectionToMongo["Coucou"]
+
