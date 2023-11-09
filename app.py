a2d3d55f0054021438fa42cf6f82b7ac3e18d8a4
Victor Campos <victormscampos@uol.com.br>
2023-11-09 13:57:25 -0800

    changed the collections name

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import json
 
 connectionToMongo = pymongo.MongoClient("mongodb+srv://ProjectCoucou:THEpassword>@cluster0.ekjezah.mongodb.net/?retryWrites=true&w=majority")
 db = connectionToMongo["Coucou"]
-
+dbcollection = db["CoucouFiles"]
