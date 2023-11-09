25776facab029f390a1c2448669d6e60aef100c5
Victor Campos <victormscampos@uol.com.br>
2023-11-09 13:51:18 -0800

    created app.py to connect with mongoDB

---
+++ app.py
@@ -0,0 +1,5 @@
+import pymongo
+import json
+
+connectionToMongo = pymongo.MongoClient("mongodb+srv://ProjectCoucou:THEpassword>@cluster0.ekjezah.mongodb.net/?retryWrites=true&w=majority")
+db = dbconnection["Coucou"]
