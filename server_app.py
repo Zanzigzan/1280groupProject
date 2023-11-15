84389fad39cd72f22eea4d9162ccc50769efaa99
Victor Campos <victormscampos@uol.com.br>
2023-11-15 13:25:16 -0800

    FirstMenu UI (NOT COMPLETE)

--- server_app.py
+++ server_app.py
@@ -1,4 +1,6 @@
 global socket
+global userInput
+global exitButton
 
 def get_message():
     return socket.recv(1024).decode('utf-8')
@@ -13,3 +15,14 @@ def app(client_socket):
     while True:
         message = get_message()
         send_message(message)
+
+
+
+def firstMenu():
+    userInput = int(input("Welcome to Coucou! Choose the option: \n1.Register\n2.Log In\n3.Exit"))
+    if userInput == 1:
+        register()
+    elif userInput == 2:
+        LogIn()
+    elif userInput == 3:
+        
