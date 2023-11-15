824b53502c8da551e63e0fcbc316770350d958f2
Zanzigzan <36047075+Zanzigzan@users.noreply.github.com>
2023-11-15 12:40:37 -0800

    Server app added

--- server_app.py
+++ server_app.py
@@ -1,6 +1,15 @@
+global socket
+
+def get_message():
+    return socket.recv(1024).decode('utf-8')
+
+def send_message(message):
+    socket.send(message.encode('utf-8'))
+
 def app(client_socket):
+    global socket
+    socket = client_socket
+    
     while True:
-        message = client_socket.recv(1024).decode('utf-8')
-        
-        if message:
-            client_socket.send(message.encode('utf-8'))
+        message = get_message()
+        send_message(message)
