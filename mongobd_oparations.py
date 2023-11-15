ca655243cc69b9e673e2549e58abebd5fe94246d
Zanzigzan <36047075+Zanzigzan@users.noreply.github.com>
2023-11-15 13:28:53 -0800

    mongodb file with desc of functions added

---
+++ mongobd_oparations.py
@@ -0,0 +1,19 @@
+def register():
+    pass
+
+def log_in():
+    pass
+
+def insert_message():
+    pass 
+
+def get_message():
+    pass
+
+def delete_message():
+    # Just change message content to "*Deleted*"
+    pass
+
+def get_chats():
+    # Should return list of all chats with logged in user
+    pass
