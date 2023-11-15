85772ffbcade71d2cc7e56327954b2dcec69257a
Zanzigzan <36047075+Zanzigzan@users.noreply.github.com>
2023-11-15 13:50:31 -0800

    Server app updated

--- server_app.py
+++ server_app.py
@@ -1,15 +1,17 @@
+import mongobd_oparations as db
+
 global socket
 global c_address
 
 # Utility functions
-def recive_message():
+def recive_from_client():
     message = socket.recv(1024).decode('utf-8')
     
     print(f"{c_address} Message \"{message}\" recived")
     
     return message
 
-def send_message(message):
+def send_to_client(message):
     print(f'{c_address} Message \"{message}\" sent')
     
     socket.send(message.encode('utf-8'))
@@ -21,17 +23,22 @@ def app(client_socket, client_address):
     socket = client_socket
     c_address = client_address
     
-    while True:
-        message = recive_message()
+    logging_menu()
+
+
+def logging_menu():
+    userInput = 0
+    
+    while (userInput != 3):
+        userInput = int(input("Welcome to Coucou! Choose the option: \n1.Register\n2.Log In\n3.Exit"))
         
-        send_message(message)
+        if userInput == 1:
+            register_menu()
+        elif userInput == 2:
+            login_menu() 
 
+def register_menu():
+    pass
 
-
-def firstMenu():
-    userInput = int(input("Welcome to Coucou! Choose the option: \n1.Register\n2.Log In\n3.Exit"))
-    if userInput == 1:
-        register()
-    elif userInput == 2:
-        LogIn()
-    elif userInput == 3:
+def login_menu():
+    pass
