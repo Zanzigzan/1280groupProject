d036b289da024cf6ec93e1cea3199774aaab4ba1
Isabellewn <isabellewn2011@gmail.com>
2024-01-13 17:42:29 -0800

    updated message

--- test_connection.py
+++ test_connection.py
@@ -9,6 +9,6 @@ client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    print("Pinged your deployment. You successfully connected to MongoDB Atlas!")
 except Exception as e:
     print(e)
